cf77062a4004f347ac0a18684f3673b44d2786e7
John Paul De Jesus <38452433+polenginlabu@users.noreply.github.com>
2025-06-25 12:15:23 +0800

    updates

--- app/Livewire/HeaderComponent.php
+++ app/Livewire/HeaderComponent.php
@@ -6,8 +6,10 @@ use Livewire\Component;
 
 class HeaderComponent extends Component
 {
+    public $slug;
     public function render()
     {
+        if($this->slug) return view("weddings.{$this->slug}.livewire.header-component");
         return view('livewire.header-component');
     }
 }
